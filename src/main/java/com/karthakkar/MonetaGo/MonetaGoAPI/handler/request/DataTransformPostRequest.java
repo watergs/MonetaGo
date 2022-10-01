5c0cccc8fe39d9b9eb3a9fdbd3ea150baea7c7f3
Karan Thakkar <karan17thakkar@gmail.com>
2022-10-02 01:59:40 +0800

    Add ToString methods for logging

--- src/main/java/com/karthakkar/MonetaGo/MonetaGoAPI/handler/request/DataTransformPostRequest.java
+++ src/main/java/com/karthakkar/MonetaGo/MonetaGoAPI/handler/request/DataTransformPostRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import java.util.List;
  */
 @Getter
 @AllArgsConstructor
+@ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class DataTransformPostRequest {
     private String orderId;
--- src/main/java/com/karthakkar/MonetaGo/MonetaGoAPI/handler/request/ItemPostRequest.java
+++ src/main/java/com/karthakkar/MonetaGo/MonetaGoAPI/handler/request/ItemPostRequest.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ItemPostRequest {
     private String itemId;
